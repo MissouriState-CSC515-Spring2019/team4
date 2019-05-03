7f6ae8e8c2e445c1c6321f99a1755329f71352bc
Fred <jhleefred@naver.com>
2019-05-02 19:33:52 -0500

    added e2e test

--- MovieDBProject/e2e/src/app.e2e-spec.ts
+++ MovieDBProject/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,14 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to LibraryDemo!');
+    //browser.pause();
+    expect(page.getTitleText()).toEqual('Movie DB Single Page Application');
+  });
+
+  it('should display Search Movie button', () => {
+    page.navigateTo();
+    //browser.pause();
+    expect(page.getSearchMovieButton().getText()).toEqual('Search Movie');
   });
 
   afterEach(async () => {
--- MovieDBProject/e2e/src/app.po.ts
+++ MovieDBProject/e2e/src/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+
+  getSearchMovieButton(){
+    return element(by.css('[routerlink="/search-movie"]'));
+  }
 }
