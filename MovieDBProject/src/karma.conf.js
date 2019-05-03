119012b07d97e2342d43af4133bf7aadf5724297
Fred <jhleefred@naver.com>
2019-05-03 10:27:13 -0500

    e2e testing

--- MovieDBProject/src/karma.conf.js
+++ MovieDBProject/src/karma.conf.js
@@ -4,14 +4,14 @@
 module.exports = function (config) {
     config.set({
       basePath: '',
-      frameworks: ['jasmine', '"@angular-devkit/build-angular'],
+      frameworks: ['jasmine'],
       plugins: [
         require('karma-jasmine'),
         require('karma-chrome-launcher'),
         //require('karma-firefox-launcher'), // npm install karma-firefox-launcher --save-dev
         require('karma-jasmine-html-reporter'),
         require('karma-coverage-istanbul-reporter'),
-        require('"@angular-devkit/build-angular/plugins/karma')
+        //require('"@angular-devkit/build-angular/plugins/karma')
       ],
       client:{
         clearContext: false // leave Jasmine Spec Runner output visible in browser
